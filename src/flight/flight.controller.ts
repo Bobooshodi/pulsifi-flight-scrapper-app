713a82d3a6d0437f5aa7b4727d2ba16863a15fc5
Duba <feyi.tobi@gmail.com>
2025-01-18 14:54:56 +0800

    Added Swagger implementation

--- src/flight/flight.controller.ts
+++ src/flight/flight.controller.ts
@@ -11,6 +11,7 @@ import { FlightService } from './flight.service';
 import { FLIGHT_TYPES } from './constants/flightTypes';
 import { SearchFlightDto } from './dto/search-flight.dto';
 import { Airport } from 'src/airport/entities/airport.entity';
+import { ApiParam, ApiQuery } from '@nestjs/swagger';
 
 @Controller('flight')
 export class FlightController {
@@ -24,6 +25,11 @@ export class FlightController {
   }
 
   @Get(':type')
+  @ApiParam({ name: 'type', required: true, type: 'string', enum: FLIGHT_TYPES, example: FLIGHT_TYPES.ROUND_TRIP })
+  @ApiQuery({ name: 'origin', required: true, type: 'string', example: 'NYCA' })
+  @ApiQuery({ name: 'destination', required: true, type: 'string', example: 'MSYA' })
+  @ApiQuery({ name: 'startDate', required: true, type: 'string', example: '2025-02-14' })
+  @ApiQuery({ name: 'endDate', required: true, type: 'string', example: '2025-02-15' })
   async search(
     @Param('type') type: FLIGHT_TYPES,
     @Query() query: SearchFlightDto,
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,16 @@ async function bootstrap() {
       inheritAppConfig: true,
     },
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Flight Scrapper Sample App')
+    .setDescription('Sample App for Flight Scrapper')
+    .setVersion('1.0')
+    .addTag('flight')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   app.useGlobalPipes(
     new ValidationPipe({
